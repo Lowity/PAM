1eeadd085e936b2d89058316ae604d9322f03205
lowity <seya.fusion@gmail.com>
2022-04-18 18:20:46 +0200

    Added Julia-Menge

--- julia-menge/julia-menge.py
+++ julia-menge/julia-menge.py
@@ -5,8 +5,8 @@ import colorsys
 import numpy as np
 from alive_progress import alive_bar
 
-width = 700
-height = 400
+width = 500
+height = 200
 
 setup(width, height)
 
@@ -17,11 +17,11 @@ if width > height:
 
 colormode(255)
 
-def calcMandelbrot(z, c, iter):
+def calcJulia(z, c, iter):
     if iter < 50 and abs(z) < 2:
         z = z**2 + c
         iter += 1
-        return calcMandelbrot(z, c, iter)
+        return calcJulia(z, c, iter)
     return iter
 
 def pix_to_complex(x, y, w, h):
@@ -54,17 +54,19 @@ def drawNumber(x, y, col_val):
     dot(1, color)
 
     
-def drawMandelbrot(w, h):
+def drawJulia(w, h, c):
     with alive_bar(w**2, title="Calculating...") as bar:
         for x in range(int(-(w/2)), int((w/2))):
             for y in range(int(-(w/2)), int((w/2))):
-                c = pix_to_complex(x, y, w, h)
-                n = calcMandelbrot(0, c, 0)
+                z = pix_to_complex(x, y, w, h)
+                n = calcJulia(z, c, 0)
                 drawNumber(x, y, n)
                 bar()
 
+c = complex(-0.5, 0.4)
+
 tracer(0,0)
-drawMandelbrot(width, height)
+drawJulia(width, height, c)
 hideturtle()
 update()
 mainloop()
